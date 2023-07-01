6c8797369d3edfdb978d65e366e8f646683a1662
Viktor Didenko <viktor_didenko@inbox.ru>
2023-07-01 15:30:18 +0300

    style: ещё один импорт

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -8,7 +8,6 @@ import ru.practicum.shareit.Create;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/users")
