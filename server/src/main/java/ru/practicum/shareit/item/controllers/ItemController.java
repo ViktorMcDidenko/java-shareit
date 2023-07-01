8c2fcacf36748243e0b89fee059fa2836ee36a8f
Viktor Didenko <viktor_didenko@inbox.ru>
2023-07-01 15:26:12 +0300

    style: убрать неиспользуемые импорты

--- server/src/main/java/ru/practicum/shareit/item/controllers/ItemController.java
+++ server/src/main/java/ru/practicum/shareit/item/controllers/ItemController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
--- server/src/main/java/ru/practicum/shareit/request/controllers/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controllers/ItemRequestController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.request.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
